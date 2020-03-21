e550ab51266520bfa9e489914137d3b5baa7e9ba
Neeraj Jaikumar <neerajjaikumar@Neerajs-MacBook-Pro.local>
2020-03-21 17:30:18 -0400

    Updated model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
     self.primary_key = 'asin'
     validates :asin, presence: true
     validates :category, presence: true
-    validates :title, presence: true
     validates :rank, presence: true
     validates :dimensions, presence: true
 end
