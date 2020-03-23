d7398b4e83e063adec9fce06bb3be88fbe86ecba
Neeraj Jaikumar <neerajjaikumar@Neerajs-MacBook-Pro.local>
2020-03-23 04:15:47 -0400

    Updated code

--- app/services/content_retriever_service.rb
+++ app/services/content_retriever_service.rb
@@ -7,23 +7,26 @@ class ContentRetrieverService
 
   def get_content
     begin
-      options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    browser = Selenium::WebDriver.for :remote, options: options
-    p 'reached here'
+    #   options = Selenium::WebDriver::Chrome::Options.new
+    # options.add_argument('--headless')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # browser = Selenium::WebDriver.for :remote, options: options
+    # p 'reached here'
 
-    browser.get path
-    p browser.page_source
-    Nokogiri::HTML.parse(browser.page_source)
-    browser.quit()
+    # browser.get path
+    # p browser.page_source
+    # Nokogiri::HTML.parse(browser.page_source)
+    # browser.quit()
+    html = open(path, "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36").read
+    Nokogiri::HTML.parse(html)
     rescue => ex
       p ex
     end
     #Selenium::WebDriver::Chrome.driver_path="/usr/bin/chromedriver"
     
   end
+
 
   private
 
