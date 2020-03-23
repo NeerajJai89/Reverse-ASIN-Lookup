6df959a7f9d242435b3f2e41fcd5f0c229c5d17c
Neeraj Jaikumar <neerajjaikumar@Neerajs-MacBook-Pro.local>
2020-03-23 08:50:13 -0400

    Updated

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe Product, type: :model do
+  subject { described_class.new }
+
+  describe '#validation', :aggregate_failures do
+    it { should validate_presence_of(:rank) }
+    it { should validate_presence_of(:asin) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:dimensions) }
+  end
 end
