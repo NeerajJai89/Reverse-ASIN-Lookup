b3674df4a6f88901d302fca3913e31f2193bcf9f
Neeraj Jaikumar <neerajjaikumar@Neerajs-MacBook-Pro.local>
2020-03-23 09:58:30 -0400

    Adding configuration for development and production

--- app/services/crawler_service.rb
+++ app/services/crawler_service.rb
@@ -8,16 +8,19 @@ class CrawlerService
 
   def get_content
 
-    api = ProxyCrawl::API.new(token: ENV['CRAWLER_TOKEN'])
-    html = api.get(url)
-    Nokogiri::HTML(html.body)
-    # options = Selenium::WebDriver::Chrome::Options.new
-    # options.add_argument('--headless')
-    # options.add_argument('--no-sandbox')
-    # browser = Selenium::WebDriver.for :chrome, options: options
-    # browser.get url
-    # Nokogiri::HTML.parse(browser.page_source)
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    browser = Selenium::WebDriver.for :chrome, options: options
+    browser.get url
+    Nokogiri::HTML.parse(browser.page_source)
 
+    # Only to be used in production. 
+    # api = ProxyCrawl::API.new(token: ENV['CRAWLER_TOKEN'])
+    # html = api.get(url)
+    # Nokogiri::HTML(html.body)
+    
+    #Nokogiri has problems parsing Amazon pages from network calls made using open-uri, tests for ASIN's with valid information led to null results, therefore used selenium instead
     # html = open(path, "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36").read
     # Nokogiri::HTML.parse(html)
     
